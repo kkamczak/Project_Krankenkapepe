5b4b5db2846a3e0ec6e81b2922b18c4b13040e80
kkamczak <kkamczak@gmail.com>
2023-11-06 20:34:28 +0100

    The PlayerStatus class has been changed. Added more encapsulation.

--- src/player.py
+++ src/player.py
@@ -292,6 +292,15 @@ class PlayerStatus():
         self.just_jumped = False
         self.can_use_object = [False, None]
 
+    def set_facing(self, new_facing: bool) -> None:
+        self.facing_right = new_facing
+
+    def set_jumped_status(self, new_status: bool) -> None:
+        self.just_jumped = new_status
+
+    def set_object_usable(self, can_use: list) -> None:
+        self.can_use_object = can_use
+
     def reset_status(self):
         self.type = 'player'
         self.id = 999
@@ -302,32 +311,32 @@ class PlayerStatus():
     def get_status(self):
         if self.player.movement.direction.y < 0 and self.get_action() == 'nothing':
             if self.status != 'jump':
-                self.player.animations.frame_index = 0
+                self.player.animations.set_frame_index(0)
             self.status = 'jump'
         elif self.player.movement.direction.y > 1 and self.get_action() == 'nothing':
             if self.status != 'fall':
-                self.player.animations.frame_index = 0
+                self.player.animations.set_frame_index(0)
             self.status = 'fall'
         elif self.get_action() == 'sword':
             if self.status != 'attack':
-                self.player.animations.frame_index = 0
+                self.player.animations.set_frame_index(0)
             self.status = 'attack'
         elif self.get_action() == 'arch':
             if self.status != 'arch':
-                self.player.animations.frame_index = 0
+                self.player.animations.set_frame_index(0)
             self.status = 'arch'
         elif self.get_action() == 'shield':
             if self.status != 'shield':
-                self.player.animations.frame_index = 0
+                self.player.animations.set_frame_index(0)
             self.status = 'shield'
         else:
             if self.player.movement.direction.x != 0:
                 if self.status != 'run':
-                    self.player.animations.frame_index = 0
+                    self.player.animations.set_frame_index(0)
                 self.status = 'run'
             else:
                 if self.status != 'idle':
-                    self.player.animations.frame_index = 0
+                    self.player.animations.set_frame_index(0)
                 self.status = 'idle'
 
     def get_action(self):
